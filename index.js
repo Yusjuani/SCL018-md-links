c3fdc8db4b8842228fb9527b5b94f9c26c2933c9
yusjuani <veroyusjuani@gmail.com>
2021-12-15 10:49:39 -0300

    leer readme

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
+//module.exports = () => {
   // ...
-};
+//};
+
+  const fs = require('fs');
+  console.log('iniciado')
+  fs.readFile('README.md', 'utf-8', (err, data) => {
+    if (err) {
+      console.log(`error ${error}`);
+    }else{
+      console.log(data);
+    }
+  
+  })
+console.log('fin')
+  
+
