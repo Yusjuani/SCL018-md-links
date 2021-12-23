19457ae435296017e20395d289334133a896da98
yusjuani <veroyusjuani@gmail.com>
2021-12-23 12:45:51 -0300

    extraer links de archivo

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 //module.exports = () => {
   // ...
 //};
-
+  //const chalk = require('chalk');
   const fs = require('fs');
-  console.log('iniciado')
-  fs.readFile('README.md', 'utf-8', (err, data) => {
-    if (err) {
-      console.log(`error ${error}`);
-    }else{
-      console.log(data);
-    }
-  
-  })
-console.log('fin')
-  
+  const markdownLinkExtractor = require("markdown-link-extractor");
+  const ruta = process.argv[2];
+
+
+  const linkExtractor = (dataFile, pathInitial) => {
+    const links = markdownLinkExtractor(dataFile, true).filter(
+      (link) => link.href.includes("https://") || link.href.includes("http://")
+    );
+    let arrayLinks = [];
+    links.forEach((link) => {
+      const objectLinks = {
+        file: pathInitial,
+        link: link.href,
+        text: link.text,
+      };
+      arrayLinks.push(objectLinks);
+    });
+    return arrayLinks;
+  };
+
+    
+  const lectura = (filePath) => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(err);
+      }else{
+        resolve(linkExtractor(data, filePath))
+      }
+    })
+  });
+}
+lectura(ruta).then(rest => {console.log(rest)});
 
